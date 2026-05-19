d7865a3b1b20a100739faefbe7ec6e16414ae426
Mayankkumar1999 <125815080+Mayankkumar1999@users.noreply.github.com>
2026-05-19 15:30:06 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,3 +9,36 @@ sed -i '56d' apache-tomcat-11.0.14/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-11.0.14/webapps/manager/META-INF/context.xml
 sed -i '22d' apache-tomcat-11.0.14/webapps/manager/META-INF/context.xml
 sh apache-tomcat-11.0.14/bin/startup.sh
+
+
+
+#bin this is another script for downloading tomcat
+
+#!/bin/bash
+
+# Install Java 21
+dnf install java-21-amazon-corretto -y
+
+# Set JAVA_HOME
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+
+# Get latest Tomcat 11 version dynamically
+VERSION=$(curl -s https://dlcdn.apache.org/tomcat/tomcat-11/ | grep -oP '(?<=v)[\d.]+' | sort -V | tail -1)
+echo "Installing Tomcat version: $VERSION"
+
+# Download Tomcat
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v${VERSION}/bin/apache-tomcat-${VERSION}.tar.gz
+
+# Extract
+tar -zxvf apache-tomcat-${VERSION}.tar.gz
+
+# Configure tomcat-users.xml (safe method, no hardcoded line numbers)
+sed -i 's|</tomcat-users>|<role rolename="manager-gui"/>\n<role rolename="manager-script"/>\n<user username="tomcat" password="root123456" roles="manager-gui,manager-script"/>\n</tomcat-users>|' apache-tomcat-${VERSION}/conf/tomcat-users.xml
+
+# Remove IP restriction for manager app
+sed -i '/allow="127\./d' apache-tomcat-${VERSION}/webapps/manager/META-INF/context.xml
+
+# Start Tomcat
+sh apache-tomcat-${VERSION}/bin/startup.sh
+
+echo "Tomcat $VERSION started! Access at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):8080"
